5fae6799c5fffa05d16400fa48657f3c7171b9cc
YeonGyu Jeong <yeongyu.jeong@devsisters.com>
2022-08-29 17:05:40 +0900

    Split controller and webhook

---
+++ src/bin/webhook.rs
@@ -0,0 +1,60 @@
+use anyhow::Result;
+use axum_server::tls_rustls::RustlsConfig;
+use checkpoint::config::WebhokConfig;
+
+/// Generate future that awaits shutdown signal
+async fn shutdown_signal(axum_server_handle: axum_server::Handle) {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    axum_server_handle.graceful_shutdown(Some(std::time::Duration::from_secs(30)));
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let config: WebhokConfig = envy::prefixed("CONF_").from_env()?;
+    let kube_config = kube::Config::infer().await?;
+    let client: kube::Client = kube_config.try_into()?;
+
+    // Prepare HTTP app
+    let http_app = checkpoint::handler::create_app(client);
+
+    // Prepare TLS config for HTTPS serving
+    let tls_config = RustlsConfig::from_pem_file(&config.cert_path, &config.key_path).await?;
+
+    // Prepare shutdown signal futures
+    let axum_server_handle = axum_server::Handle::new();
+    let shutdown_signal_fut = shutdown_signal(axum_server_handle.clone());
+    tokio::spawn(async move {
+        shutdown_signal_fut.await;
+    });
+
+    // Spawn HTTP server
+    axum_server::bind_rustls(config.listen_addr.parse()?, tls_config)
+        .handle(axum_server_handle)
+        .serve(http_app.into_make_service())
+        .await?;
+
+    Ok(())
+}
--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,13 @@
 use std::path::PathBuf;
 
-use once_cell::sync::Lazy;
 use serde::Deserialize;
-
-pub static CONFIG: Lazy<Config> = Lazy::new(|| envy::prefixed("CONF_").from_env().unwrap());
 
 fn default_listen_addr() -> String {
     "0.0.0.0:3000".to_string()
 }
 
 #[derive(Deserialize, Clone, Debug)]
-pub struct Config {
+pub struct ControllerConfig {
     /// Installed Kubernetes Service namespace of the controller
     pub service_namespace: String,
     /// Installed Kubernetes Service name of the controller
@@ -18,13 +15,16 @@ pub struct Config {
     /// Installed Kubernetes Service port of the controller
     pub service_port: i32,
 
+    /// Base64 encoded PEM CA bundle for controller
+    pub ca_bundle: String,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct WebhokConfig {
     /// Certificate path for HTTPS
     pub cert_path: PathBuf,
     /// Certificate key path for HTTPS
     pub key_path: PathBuf,
-
-    /// Base64 encoded PEM CA bundle for webhook config
-    pub ca_bundle: String,
 
     #[serde(default = "default_listen_addr")]
     pub listen_addr: String,
