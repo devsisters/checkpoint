bdf44ababd638bcc9b041ada59ef0b9e220395ea
Junghoon Noh <junghoon.noh@devsisters.com>
2022-11-03 13:33:58 +0900

    Add cert-manager support in webhook and chart (#47)

--- src/bin/webhook.rs
+++ src/bin/webhook.rs
@@ -1,6 +1,12 @@
+use std::io;
+use std::path::Path;
+
 use anyhow::Result;
 use axum_server::tls_rustls::RustlsConfig;
-use checkpoint::config::WebhokConfig;
+use notify::{RecursiveMode, Watcher};
+use tokio::runtime::Runtime;
+
+use checkpoint::config::WebhookConfig;
 
 /// Generate future that awaits shutdown signal
 async fn shutdown_signal(axum_server_handle: axum_server::Handle) {
@@ -31,11 +37,17 @@ async fn shutdown_signal(axum_server_handle: axum_server::Handle) {
     axum_server_handle.graceful_shutdown(Some(std::time::Duration::from_secs(30)));
 }
 
+async fn reload_config(config: &WebhookConfig, tls_config: &RustlsConfig) -> Result<(), io::Error> {
+    tls_config
+        .reload_from_pem_file(&config.cert_path, &config.key_path)
+        .await
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let config: WebhokConfig = envy::prefixed("CONF_").from_env()?;
+    let config: WebhookConfig = envy::prefixed("CONF_").from_env()?;
     let kube_config = kube::Config::infer().await?;
     let client: kube::Client = kube_config.try_into()?;
 
@@ -44,6 +56,32 @@ async fn main() -> Result<()> {
 
     // Prepare TLS config for HTTPS serving
     let tls_config = RustlsConfig::from_pem_file(&config.cert_path, &config.key_path).await?;
+
+    let watcher_tls_config = tls_config.clone();
+    let watcher_config = config.clone();
+    let watcher_async_runtime = Runtime::new().unwrap();
+    let mut watcher = notify::recommended_watcher(move |res| {
+        tracing::info!("Reloading TLS certificate");
+        match res {
+            Ok(_) => {
+                let reload_res = watcher_async_runtime
+                    .block_on(reload_config(&watcher_config, &watcher_tls_config));
+                match reload_res {
+                    Ok(_) => {
+                        tracing::info!("TLS certificate reloaded");
+                    }
+                    Err(error) => {
+                        tracing::error!(%error, "Failed to reload cert");
+                    }
+                }
+            }
+            Err(error) => {
+                tracing::error!(%error, "Failed to watch cert");
+            }
+        }
+    })?;
+    watcher.watch(Path::new(&config.cert_path), RecursiveMode::NonRecursive)?;
+    watcher.watch(Path::new(&config.key_path), RecursiveMode::NonRecursive)?;
 
     // Prepare shutdown signal futures
     let axum_server_handle = axum_server::Handle::new();
